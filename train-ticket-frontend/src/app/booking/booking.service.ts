51305a474a505b9e2b256d61e64f1e3edadc79f1
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2025-06-12 12:35:07 +0530

    Made Train search mobile responsive

--- train-ticket-frontend/src/app/booking/booking.service.ts
+++ train-ticket-frontend/src/app/booking/booking.service.ts
@@ -4,24 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class BookingService {
-  private bookingData: any;
+  private selectedTrain: any;
 
   constructor() {}
 
-  setBookingData(data: any) {
-    this.bookingData = data;
-    localStorage.setItem('bookingData', JSON.stringify(data)); // optional for reload persistence
+  setSelectedTrain(train: any): void {
+    this.selectedTrain = train;
   }
 
-  getBookingData() {
-    if (this.bookingData) return this.bookingData;
-
-    const data = localStorage.getItem('bookingData');
-    return data ? JSON.parse(data) : null;
-  }
-
-  clearBookingData() {
-    this.bookingData = null;
-    localStorage.removeItem('bookingData');
+  getSelectedTrain(): any {
+    return this.selectedTrain;
   }
 }
